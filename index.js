00ba0921ca6cec200a8fd4b3a8652a3608bed063
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-10-07 14:15:39 +1000

    feat: add task 3.13 Phonebook database, step1 solution

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require("express");
+require("dotenv").config();
+
 const cors = require("cors");
+const express = require("express");
 const morgan = require("morgan");
+
+const Person = require("./models/person");
 
 const app = express();
 
@@ -30,42 +34,20 @@ const generateId = () => {
   return Math.round(Math.random() * 10000);
 };
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((result) => {
+    res.json(result);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch(() => {
+      res.status(404).end();
+    });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -108,14 +90,20 @@ app.post("/api/persons", (req, res) => {
 });
 
 app.get("/info", (req, res) => {
-  const data = `
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>
-  `;
-  res.send(data);
+  Person.find({}, "id")
+    .then((result) => {
+      const data = `
+      <p>Phonebook has info for ${result.length} people</p>
+      <p>${new Date()}</p>
+      `;
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(404).end();
+    });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
